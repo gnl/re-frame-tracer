b8c33a9e0bf03f4b1eab9f5479101006a44c5794
gnl <gnl@lipov.net>
2018-06-27 23:45:27 +0300

    Unsnapshottify

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gnl/re-frame-tracer "0.1.7-SNAPSHOT"
+(defproject gnl/re-frame-tracer "0.1.7"
   :description "Tracer for clairvoyant that is optimised for cljs-devtools"
   :url "http://github.com/day8/re-frame-tracer"
   :license {:name "MIT"}
